a1facaedd8d043dcf53a4d2e6bc0bc386e1054f3
Casimir Kuusela <crk.kuusela@gmail.com>
2017-01-21 00:12:53 +0200

    Added pisteet.cs

---
+++ Assets/Scripts/pisteet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pisteet : MonoBehaviour {
+
+    
+    public int points;
+ 
+
+    // Use this for initialization
+    void Start () {
+
+        points = 0;
+		
+	}
+
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "ananas") {
+            points +=  10;
+        }
+        Destroy(other.gameObject);
+        Debug.Log(points);
+    }
+
+    private void Update()
+    {
+       
+    }
+}
