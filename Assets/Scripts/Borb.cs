a2ff46c9de481a37acf60254024e2dc69e4cac74
Pavel Smirnov <pasmpasm@cs.helsinki.fi>
2017-01-20 23:23:38 +0200

    bounce from borders

--- Assets/Scripts/Borb.cs
+++ Assets/Scripts/Borb.cs
@@ -6,16 +6,28 @@ public class Borb : MonoBehaviour {
     public float rotationSpeed;
     public float moveSpeed;
     public int playerId;
+    public float rightBorder;
+    public float leftBorder;
+    public float topBorder;
+    public float bottomBorder;
 
     private bool clockwise;
+    private string keyName;
+
 	// Use this for initialization
 	void Start () {
         clockwise = true;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        string keyName = "Player" + playerId;
+        keyName = "Player" + playerId;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        HandleButton();
+        CheckBorder();
+    }
+
+    void HandleButton()
+    {
         if (!Input.GetButton(keyName))
         {
             transform.Translate(Vector3.up * moveSpeed);
@@ -28,5 +40,29 @@ public class Borb : MonoBehaviour {
         {
             clockwise = !clockwise;
         }
-	}
+    }
+
+    void CheckBorder()
+    {
+        if (transform.position.x > rightBorder)
+        {
+            transform.position = new Vector3(rightBorder, transform.position.y, 0);
+            transform.rotation = Quaternion.Euler(0, 0, -transform.rotation.eulerAngles.z);
+        }
+        if (transform.position.x < leftBorder)
+        {
+            transform.position = new Vector3(leftBorder, transform.position.y, 0);
+            transform.rotation = Quaternion.Euler(0, 0, -transform.rotation.eulerAngles.z);
+        }
+        if (transform.position.y < bottomBorder)
+        {
+            transform.position = new Vector3(transform.position.x, bottomBorder, 0);
+            transform.rotation = Quaternion.Euler(0, 0, 180 - transform.rotation.eulerAngles.z);
+        }
+        if (transform.position.y > topBorder)
+        {
+            transform.position = new Vector3(transform.position.x, topBorder, 0);
+            transform.rotation = Quaternion.Euler(0, 0, 180 - transform.rotation.eulerAngles.z);
+        }
+    }
 }
