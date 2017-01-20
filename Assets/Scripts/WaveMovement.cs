bf4bca97c012287302f6ff46c376416b6c121893
Mazax <matias.koskinen@hotmail.com>
2017-01-20 23:34:19 +0200

    Moving Wave \o/

---
+++ Assets/Scripts/WaveMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveMovement : MonoBehaviour {
+	
+	public float waveSpeed;
+
+	[HideInInspector]
+	public float defaultWaveSpeed = 5;
+
+	// Use this for initialization
+	void Start () {
+		if (waveSpeed == null) {
+			waveSpeed = defaultWaveSpeed;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.Translate(new Vector3(0, waveSpeed,0));	
+	}
+}
---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		SpawnWave ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void SpawnWave(){
+		GameObject wave = (GameObject)Instantiate(Resources.Load("Wave"));
+		wave.transform.Translate(new Vector3(0, 1080, 0));
+	}
+}
