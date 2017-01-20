144e47c327d18112dd1962647552b1889bd70930
Pavel Smirnov <pasmpasm@cs.helsinki.fi>
2017-01-20 21:25:40 +0200

    first borb

---
+++ Assets/Scripts/Borb.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Borb : MonoBehaviour {
+    public float rotationSpeed;
+    public float moveSpeed;
+
+    private bool clockwise;
+	// Use this for initialization
+	void Start () {
+        clockwise = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!Input.GetButton("Fire1"))
+        {
+            transform.Translate(Vector3.up * moveSpeed);
+            if (clockwise)
+                transform.Rotate(Vector3.forward, -rotationSpeed);
+            else
+                transform.Rotate(Vector3.forward, rotationSpeed);
+        }
+        if (Input.GetButtonDown("Fire1"))
+        {
+            clockwise = !clockwise;
+        }
+	}
+}
