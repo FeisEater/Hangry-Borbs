27c0b10eebb0696519f6b0c148f3d13471603ac5
Pavel Smirnov <pasmpasm@cs.helsinki.fi>
2017-01-21 14:44:41 +0200

    some changes i dont remember

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour {
+    private float time;
+	// Use this for initialization
+	void Start () {
+        GetComponent<SpriteRenderer>().color = new Color(1,1,1,0);
+        time = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (time < 1)
+        {
+            time += Time.deltaTime * 2;
+            GetComponent<SpriteRenderer>().color = Color.Lerp(new Color(1, 1, 1, 0), Color.white, time);
+        }
+    }
+}
