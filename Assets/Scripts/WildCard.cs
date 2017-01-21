a8ad26afa4031a1b1d839c9a7afffe78541e5fe1
Pavel Smirnov <pasmpasm@cs.helsinki.fi>
2017-01-21 22:14:28 +0200

    wildcard bitches

---
+++ Assets/Scripts/WildCard.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WildCard : MonoBehaviour {
+    public float BadProbability;
+    public int points;
+    public float timeToEat;
+	// Use this for initialization
+	void Start () {
+		if (Random.value < BadProbability)
+        {
+            GetComponent<Item>().isOneWaveItem = true;
+        }
+        else
+        {
+            Food food = gameObject.AddComponent<Food>();
+            food.points = points;
+            food.timeToEat = timeToEat;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
