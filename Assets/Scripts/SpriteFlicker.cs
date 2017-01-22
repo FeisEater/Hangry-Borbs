d4e14e3befebaf542fa3a1d4c11c4400a4299579
Mazax <matias.koskinen@hotmail.com>
2017-01-22 15:47:14 +0200

    Add MainMenu scene.

---
+++ Assets/Scripts/SpriteFlicker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteFlicker : MonoBehaviour {
+
+	public Sprite sprite1;
+	public Sprite sprite2;
+	public float flickerTime;
+	private float changeSpriteAtTime;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > changeSpriteAtTime) {
+			if (gameObject.GetComponent<SpriteRenderer> ().sprite == sprite1) {
+				gameObject.GetComponent<SpriteRenderer> ().sprite = sprite2;
+			}else {
+				gameObject.GetComponent<SpriteRenderer> ().sprite = sprite1;
+			}
+			changeSpriteAtTime = Time.time + flickerTime;
+		}
+	}
+}
