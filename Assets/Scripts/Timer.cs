a7040bb7bd0bff913387abcf071c69bea2a4c96d
Casimir Kuusela <crk.kuusela@gmail.com>
2017-01-22 12:09:27 +0200

    Timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public float timeLeft;
+
+    public Text text;
+
+
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        text.text = "Time Left:" + Mathf.Round(timeLeft);
+        if (timeLeft < 0)
+        {
+            Debug.Log("GAMEOVER");
+            //Application.LoadLevel("gameOver");
+        }
+    }
+}
