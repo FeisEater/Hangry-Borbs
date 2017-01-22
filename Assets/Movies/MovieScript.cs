e10a9a5030a08a23e96a065805e54ff34e0ef81b
Pavel Smirnov <pasmpasm@cs.helsinki.fi>
2017-01-22 02:51:24 +0200

    leffa

---
+++ Assets/Movies/MovieScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MovieScript : MonoBehaviour {
+    public int nextSceneIndex;
+    private MovieTexture movie;
+    private bool canSkip;
+	// Use this for initialization
+	void Start () {
+        movie = ((MovieTexture)GetComponent<RawImage>().mainTexture);
+        movie.Play();
+        canSkip = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (!movie.isPlaying || (canSkip && Input.GetKeyDown(KeyCode.Space)))
+        {
+            Application.LoadLevel(nextSceneIndex);
+        }
+        if (Input.anyKeyDown)
+        {
+            canSkip = true;
+            GameObject.Find("Skip Message").GetComponent<Text>().enabled = true;
+        }
+    }
+}
